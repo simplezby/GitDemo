b08f1d3aa8de54846a8beed9a34a3953cab925d5
zby <943172625@qq.com>
2021-10-05 20:58:35 +0800

    hot first commit

--- src/main/java/com/zby/Person.java
+++ src/main/java/com/zby/Person.java
@@ -5,4 +5,8 @@ public class Person {
     public String name;
     private int age;
     private int birthday;
+
+    public static void main(String[] args) {
+        System.out.println("111111111111111");
+    }
 }
