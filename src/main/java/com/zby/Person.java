97797fff8abd3025c6162117905bfac4f34017c7
simplezby <74749358+simplezby@users.noreply.github.com>
2021-10-05 21:59:09 +0800

    1

--- src/main/java/com/zby/Person.java
+++ src/main/java/com/zby/Person.java
@@ -8,7 +8,7 @@ public class Person {
 
     public static void main(String[] args) {
         System.out.println("hot test");
-
+          System.out.println("master test");
         System.out.println("master test");
 
     }
